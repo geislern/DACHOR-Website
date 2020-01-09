51ba203d4259871a634f83352a462bc7916ffcd4
N. Geisler <12515934+geislern@users.noreply.github.com>
2020-01-09 18:22:47 +0100

    add bash utility scripts
    add setup script
    add update script
    add check script

---
+++ Utils/setup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# Setup AKPlanning
+# execute as Utils/check.sh
+
+# abort on error, print executed commands
+set -ex
+
+# remove old virtualenv
+rm -rf venv/
+
+# Setup Python Environment
+# Requires: Virtualenv, appropriate Python installation
+virtualenv venv -p python3.7
+source venv/bin/activate
+pip install --upgrade setuptools pip wheel
+pip install -r requirements.txt
+
+# Setup database
+python manage.py migrate
+
+# Prepare static files and translations
+python manage.py collectstatic --noinput
+python manage.py compilemessages
+
+# Create superuser
+# Credentials are entered interactively on CLI
+python manage.py createsuperuser
+
+deactivate
