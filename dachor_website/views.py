409f8174a11ca91a9d54baf0621e473b4f5e22cf
N. Geisler <12515934+geislern@users.noreply.github.com>
2020-01-13 18:30:46 +0100

    setup basic template view as starting page
    import urls from dachor_website app
    create url configuration for dachor_website app
    add basic template view for root path
    create starting page html template using bootstrap4

--- dachor_website/views.py
+++ dachor_website/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.views.generic import TemplateView
+
+
+class HomepageView(TemplateView):
+    template_name = 'dachor_website/homepage.html'
