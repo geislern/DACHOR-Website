6bee3ca0d895551337585dc64d5e95eef84a1295
N. Geisler <12515934+geislern@users.noreply.github.com>
2020-01-14 15:59:05 +0100

    create page structure
    add templates for all sites
    fill links
    configure urls
    create template views

--- dachor_website/urls.py
+++ dachor_website/urls.py
@@ -5,5 +5,11 @@ from . import views
 app_name = 'website'
 
 urlpatterns = [
-    path('', views.HomepageView.as_view(), name='homepage')
+    path('', views.HomepageView.as_view(), name='homepage'),
+    path('ueber_uns/', views.AboutView.as_view(), name='about'),
+    path('kontakt/', views.ContactView.as_view(), name='contact'),
+    path('auftritte/', views.EventsView.as_view(), name='events'),
+    path('impressum/', views.ImpressView.as_view(), name='impress'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('datenschutz/', views.PrivacyView.as_view(), name='privacy'),
 ]
--- dachor_website/views.py
+++ dachor_website/views.py
@@ -4,3 +4,27 @@ from django.views.generic import TemplateView
 
 class HomepageView(TemplateView):
     template_name = 'dachor_website/homepage.html'
+
+
+class AboutView(TemplateView):
+    template_name = 'dachor_website/about.html'
+
+
+class ContactView(TemplateView):
+    template_name = 'dachor_website/contact.html'
+
+
+class EventsView(TemplateView):
+    template_name = 'dachor_website/events.html'
+
+
+class ImpressView(TemplateView):
+    template_name = 'dachor_website/impress.html'
+
+
+class LoginView(TemplateView):
+    template_name = 'dachor_website/login.html'
+
+
+class PrivacyView(TemplateView):
+    template_name = 'dachor_website/privacy.html'
