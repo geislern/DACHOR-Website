9720f53d4433dadc676256deafe192bbeefd2479
N. Geisler <12515934+geislern@users.noreply.github.com>
2020-01-16 17:15:28 +0100

    automatically use python 3.8 in setup script

--- Utils/setup.sh
+++ Utils/setup.sh
@@ -10,7 +10,7 @@ rm -rf venv/
 
 # Setup Python Environment
 # Requires: Virtualenv, appropriate Python installation
-virtualenv venv -p python3.7
+virtualenv venv -p python3.8
 source venv/bin/activate
 pip install --upgrade setuptools pip wheel
 pip install -r requirements.txt
