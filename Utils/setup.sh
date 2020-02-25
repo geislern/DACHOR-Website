d2182f6868e028daf832f66949f99180757bfeaa
N. Geisler <12515934+geislern@users.noreply.github.com>
2020-02-25 15:17:42 +0100

    downgrade to python3.6 for server compatibility

--- Utils/setup.sh
+++ Utils/setup.sh
@@ -10,7 +10,7 @@ rm -rf venv/
 
 # Setup Python Environment
 # Requires: Virtualenv, appropriate Python installation
-virtualenv venv -p python3.8
+virtualenv venv -p python3.6
 source venv/bin/activate
 pip install --upgrade setuptools pip wheel
 pip install -r requirements.txt
