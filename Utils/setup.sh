be7dfa2ea2ffb496fde666dc34ea33c55ec6ec55
N. Geisler <12515934+geislern@users.noreply.github.com>
2022-10-30 18:56:34 +0100

    adapt setup command for virtualenv

--- Utils/setup.sh
+++ Utils/setup.sh
@@ -10,7 +10,7 @@ rm -rf venv/
 
 # Setup Python Environment
 # Requires: Virtualenv, appropriate Python installation
-virtualenv venv -p python3.10
+python3.10 -m venv venv
 source venv/bin/activate
 pip install --upgrade setuptools pip wheel
 pip install -r requirements.txt
