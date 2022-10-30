dec0bee1630d95f1ec5fa6fa07879d234d660644
N. Geisler <12515934+geislern@users.noreply.github.com>
2022-10-30 19:21:52 +0100

    move test execution to script

---
+++ Utils/test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# run unit tests
+# execute as Utils/test.sh
+
+# activate virtualenv when necessary
+if [ -z ${VIRTUAL_ENV+x} ]; then
+    source venv/bin/activate
+fi
+
+# enable really all warnings, some of them are silenced by default
+if [[ "$@" == *"--all"* ]]; then
+    export PYTHONWARNINGS=all
+fi
+
+./manage.py test
