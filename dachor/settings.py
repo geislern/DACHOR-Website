82dd23451e0dd980e04460e3eedfe8b72bb8321e
Benjamin Haettasch <h@ttas.ch>
2022-10-31 00:05:42 +0100

    Set default language to German

--- dachor/settings.py
+++ dachor/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'de-de'
 
 TIME_ZONE = 'UTC'
 
