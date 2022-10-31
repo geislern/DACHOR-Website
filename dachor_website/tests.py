d2808fac5cad884de0a89a3286110612e1ce6f06
Benjamin Haettasch <h@ttas.ch>
2022-10-31 13:56:21 +0100

    Introduce basic tests for website component

--- dachor_website/tests.py
+++ dachor_website/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class WebsiteTests(TestCase):
+    """
+    Test the website component
+    """
+
+    VIEWS = [
+        'homepage', 'about', 'contact', 'events', 'impress', 'privacy'
+    ]
+
+    def testViews(self):
+        """
+        Test whether opening a view returns an error
+        """
+        client = Client()
+
+        for v in self.VIEWS:
+            response = client.get(reverse('website:'+v))
+            self.assertEquals(response.status_code, 200,
+                              f"Loading view '{v}' produced error {response.status_code}")
