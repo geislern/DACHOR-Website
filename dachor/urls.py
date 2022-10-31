1e2b3ada72a6e78c97d5526bceac0c2b3bb0e2f3
Benjamin Haettasch <h@ttas.ch>
2022-10-31 12:51:54 +0100

    Adjust urls to new app structure and activate app

--- dachor/urls.py
+++ dachor/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('dachor_website.urls', namespace='website')),
+    path('', include('dachor_internal.urls', namespace='internal')),
 ]
