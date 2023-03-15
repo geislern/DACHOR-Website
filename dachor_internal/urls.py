d66c6ccba32e5dcd9e7b02ba709297d017661069
Benjamin Haettasch <h@ttas.ch>
2023-03-15 10:26:23 +0100

    Add view to edit profile information

--- dachor_internal/urls.py
+++ dachor_internal/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.StartView.as_view(), name='start'),
     path('signup/', views.SignupView.as_view(), name='signup'),
     path('not-confirmed/', views.NotConfirmedView.as_view(), name='not-confirmed'),
+    path('profile/edit/', views.ProfileEditView.as_view(), name='profile-edit'),
 ]
--- dachor_internal/views.py
+++ dachor_internal/views.py
@@ -1,8 +1,9 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView, CreateView, FormView
 
-from dachor_internal.forms import SignupForm
+from dachor_internal.forms import SignupForm, ProfileForm
 from dachor_website.views import NavbarHighlightMixin
 
 
@@ -16,8 +17,25 @@ class NotConfirmedView(NavbarHighlightMixin, TemplateView):
     navbar_active = 'internal'
 
 
-class SignupView(CreateView):
+class SignupView(NavbarHighlightMixin, CreateView):
     form_class = SignupForm
     template_name = 'dachor_internal/signup.html'
     success_url = reverse_lazy('internal:not-confirmed')
+    navbar_active = 'internal'
 
+
+class ProfileEditView(NavbarHighlightMixin, FormView):
+    form_class = ProfileForm
+    template_name = 'dachor_internal/edit_profile.html'
+    success_url = reverse_lazy('internal:profile-edit')
+    navbar_active = 'internal'
+
+    def get_form_kwargs(self):
+        kwargs =  super().get_form_kwargs()
+        kwargs.update(instance=self.request.user.profile)
+        return kwargs
+
+    def form_valid(self, form):
+        form.save()
+        messages.add_message(self.request, messages.SUCCESS, "Informationen aktualisiert")
+        return super().form_valid(form)
