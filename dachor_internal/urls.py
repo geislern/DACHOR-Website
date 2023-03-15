c255026bdf297bfeabb932ea7a8c83f7ee103664
Benjamin Haettasch <h@ttas.ch>
2023-03-15 09:31:21 +0100

    Implement self-signup
    Add django-betterforms as dependency to handle whole signup process in one form, see https://django-betterforms.readthedocs.io/en/latest/multiform.html#working-with-createview
    Create forms for user information and profile including custom required fields
    Add views for signup and to inform about missing confirmation
    Link signup form in login view
    This implements #56

--- dachor_internal/urls.py
+++ dachor_internal/urls.py
@@ -6,4 +6,6 @@ app_name = 'internal'
 
 urlpatterns = [
     path('', views.StartView.as_view(), name='start'),
+    path('signup/', views.SignupView.as_view(), name='signup'),
+    path('not-confirmed/', views.NotConfirmedView.as_view(), name='not-confirmed'),
 ]
--- dachor_internal/views.py
+++ dachor_internal/views.py
@@ -1,10 +1,23 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView
 
+from dachor_internal.forms import SignupForm
 from dachor_website.views import NavbarHighlightMixin
 
 
 class StartView(NavbarHighlightMixin, LoginRequiredMixin, TemplateView):
     template_name = 'dachor_internal/index.html'
     navbar_active = 'internal'
+
+
+class NotConfirmedView(NavbarHighlightMixin, TemplateView):
+    template_name = 'dachor_internal/not_confirmed.html'
+    navbar_active = 'internal'
+
+
+class SignupView(CreateView):
+    form_class = SignupForm
+    template_name = 'dachor_internal/signup.html'
+    success_url = reverse_lazy('internal:not-confirmed')
 
